106c26823b2adc0fde83717923e4b76659985b18
Shevki S <sesy@kth.se>
2025-04-05 15:01:17 +0200

    Lagt till och uppdaterat filer

--- source/GameModel.h
+++ source/GameModel.h
@@ -4,22 +4,45 @@
 #include <SDL2/SDL.h>
 
 #define PLAYER_COUNT 6
-#define PLAYER_SIZE 50
+#define PLAYER_SIZE 64
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+typedef enum {
+    IDLE = 0,
+    WALK,
+    RUN,
+    JUMP,
+    SLIDE,
+    SHOOT,
+    ANIMATION_COUNT
+} AnimationState;
+
 typedef struct {
     float x, y;
+    int frame;
+    Uint32 lastFrameTime;
+    int animationState;
+    float angle;
 } Player;
 
+typedef struct {
+    int x, y;
+    int width, height;
+    SDL_Texture *texture;
+} Grass;
+
 typedef struct GameModel {
-    Player players[PLAYER_COUNT];  // Spelare i trianglar
-    Player coach;                  // Tränarens position
+    Player players[PLAYER_COUNT];  // Spelare
+    Player coach;                  // Bollen (eller coachen)
+    SDL_Texture* coachTexture;
     int passOrder[PLAYER_COUNT];   // Passningsordning
-    int step;                      // Aktuell passningssteg
-    Uint32 lastPassTime;          // Tid för senaste pass
+    int step;                      // Aktuellt passningssteg
+    Uint32 lastPassTime;           // Tid för senaste pass
+    Grass grass;
 } GameModel;
 
-void initializeModel(struct GameModel* model);
+void cleanupModel(struct GameModel* model);
+void initializeModel(struct GameModel* model, SDL_Renderer* renderer);
 
 #endif
--- source/GameView.h
+++ source/GameView.h
@@ -4,10 +4,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-// Forward declare GameModel struct
 struct GameModel;
-
 SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* path);
-void renderGame(SDL_Renderer* renderer, SDL_Texture* playerTexture, struct GameModel* model);
+void renderGame(SDL_Renderer* renderer, SDL_Texture* playerTexture, SDL_Texture* grassTexture, struct GameModel* model);
 
 #endif
